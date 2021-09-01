25ebbdd0c30409b17e57abfe90c143c0febf6aa2
suzantadha <72731923+suzantadha@users.noreply.github.com>
2021-09-01 14:41:35 +0530

    Add files via upload

---
+++ Project-C28-template-main/js/launcher.js
@@ -0,0 +1,30 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 20
+        }    
+       
+        this.pointB = pointB
+        this.launch = Constraint.create(options);
+        World.add(world, this.launch);
+    }
+
+    fly(){
+        this.launch.bodyA = null;
+    }
+
+    display(){      
+        if(this.launch.bodyA){
+            var pointA = this.launch.bodyA.position;
+            var pointB = this.pointB;
+            
+            strokeWeight(4);
+            fill("red");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
